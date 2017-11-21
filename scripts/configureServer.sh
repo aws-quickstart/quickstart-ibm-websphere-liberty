6c2467ed944a91fd51a54f3d635ff31d4063093f
Madhuri Susarla <susarlam@amazon.com>
2017-11-21 23:03:56 +0530

    End-to-end SSL implementation and arranged parameters in alphabetical

--- scripts/configureServer.sh
+++ scripts/configureServer.sh
@@ -4,9 +4,16 @@
                       
 #Modify the entries in server.xml to add the HOST access to internet
                       
-cp $WLP_HOME/usr/servers/defaultServer/server.xml s1.xml
-                       
-sed '11 a \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \  host = "\*" ' s1.xml > $WLP_HOME/usr/servers/defaultServer/server.xml
+#cp $WLP_HOME/usr/servers/defaultServer/server.xml s1.xml
+
+content='   <featureManager>\
+       <feature>ssl-1.0</feature>\
+     </featureManager>\
+     <keyStore id=\"defaultKeyStore\" password=\"Liberty\" \/>'
+
+sed '/<!-- To access/i\'"$content" server.xml> s1.xml
+
+sed '15 a \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \  host = "\*" ' s1.xml > $WLP_HOME/usr/servers/defaultServer/server.xml
 
 rm -rf s1.xml
 
--- scripts/deploySampleApp.sh
+++ scripts/deploySampleApp.sh
@@ -2,7 +2,6 @@
 # Setup Liberty environment
 . /etc/profile.d/wlp.sh
                       
-#Modify the entries in server.xml to add the HOST access to internet                      
 
 # Deploy ferret sample application
 mv /tmp/ferret-1.0.war $WLP_HOME/usr/servers/defaultServer/dropins/ferret-1.0.war
