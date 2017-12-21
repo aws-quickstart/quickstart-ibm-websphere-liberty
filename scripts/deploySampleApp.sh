6c55e29826af7c28482be70905071bc471471ee9
Shivansh Singh <sshvans@amazon.com>
2017-12-21 10:56:02 -0800

    Add all files after submodule cleanup

---
+++ scripts/deploySampleApp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Setup Liberty environment
+. /etc/profile.d/wlp.sh
+                      
+
+# Deploy ferret sample application
+mv /tmp/ferret-1.0.war $WLP_HOME/usr/servers/defaultServer/dropins/ferret-1.0.war
+                      
---
+++ scripts/startServer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Setup Liberty environment
+. /etc/profile.d/wlp.sh
+                      
+                      
+# Start the WLP server
+service wlp start
